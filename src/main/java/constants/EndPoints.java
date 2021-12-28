ff5c88a8e73c7db1f1bb9b3c772e5fd81ac26611
AkshayNar <68892476+AkshayNar@users.noreply.github.com>
2021-12-28 17:16:54 +0530

    Update EndPoints.java

--- src/main/java/constants/EndPoints.java
+++ src/main/java/constants/EndPoints.java
@@ -4,14 +4,6 @@ public class EndPoints {
 	
 	public static final String ADD_PLACE = "/maps/api/place/add/json";
 	public static final String GET_PLACE = "/maps/api/place/get/json";
-	
-<<<<<<< HEAD
-	public static final String PLACE = "/maps/api";
-	public static final String PLACE_1 = "/maps/api";
-=======
-	public static final String PLACE = "/maps/api/place";
-	
->>>>>>> 00888b36e3c5b2c861ba4885c9b3e8c99871ebc8
 	public static final String UPDATE_PLACE = "/maps/api/place/update/json";
 	public static final String DELETE_PLACE = "/maps/api/place/delete/json";
 
